baf5bdedd6dec9240a5c38c8291609a7d07d4a42
khalidhassan3011 <khalidhassan3011@gmail.com>
2023-05-05 22:18:59 +0600

    logo change

--- lib/app/common/widgets/custom_menu.dart
+++ lib/app/common/widgets/custom_menu.dart
@@ -19,7 +19,7 @@ class CustomMenu {
           children: [
             _menuItem(
               context,
-              [UserType.client, UserType.employee],
+              [UserType.client],
               Icons.person,
               "Profile",
               onProfileTap ?? () {},
