771344b91ab2be656a673754bc84b90aff5abf7f
khalidhassan3011 <khalidhassan3011@gmail.com>
2023-05-20 14:13:53 +0600

    change server url

--- lib/app/repository/api_urls.dart
+++ lib/app/repository/api_urls.dart
@@ -3,5 +3,5 @@ part of 'api_helper_impl.dart';
 class _ApiUrls {
   _ApiUrls._();
 
-  static const String base = 'http://44.204.212.181:8000/api/v1/';
+  static const String base = 'https://server.mhpremierstaffingsolutions.com/api/v1/';
 }
